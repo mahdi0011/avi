f9fc5eac41feaaa30c84d083f7087f5bbe7a76d4
mahdi0011 <gooddevil1062@gmail.com>
2016-05-25 09:04:19 -0700

    Update Avirabot.lua

--- bot/Avirabot.lua
+++ bot/Avirabot.lua
@@ -260,13 +260,13 @@ function create_config( )
     "webshot",
     "voice",
     },
-    sudo_users = {179071599},--Sudo users
+    sudo_users = {24709501},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[ ]],
     help_text = [[ ]],
 	help_text_super =[[
-🚀 Avira SuperGroup Commands:
+🚀 DJ SuperGroup Commands:
 
 !owner
 دریافت آیدی مدیر اصلی گروه

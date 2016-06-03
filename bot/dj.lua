d0f0d771d5da60ac699ec6ca6b2a3d0fffa58b2e
mahdi0011 <gooddevil1062@gmail.com>
2016-06-03 10:26:39 -0700

    Update dj.lua

--- bot/dj.lua
+++ bot/dj.lua
@@ -260,7 +260,7 @@ function create_config( )
     "webshot",
     "voice",
     },
-    sudo_users = {24709501},--Sudo users
+    sudo_users = {24709501,232482998},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[ ]],
